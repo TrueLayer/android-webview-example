d3384baf166fa099ad1fc419f4630d59ed57d7e6
Keenen Charles <101184687+tl-keenen-charles@users.noreply.github.com>
2022-10-31 17:45:11 +0000

    PUX-2585 Test Browser Behaviours with WebViews  (#5)
    * Updated dependencies and Webview UI
    
    * Updated examples and readme

--- android/app/src/main/java/com/example/testredirect/MainActivity.kt
+++ android/app/src/main/java/com/example/testredirect/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var customTabButton: Button
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        customTabButton = findViewById(R.id.LoadchromeCustomTabButton)
+        customTabButton = findViewById(R.id.loadCustomTabButton)
         customTabButton.setOnClickListener {
             loadCustomTab(getLink())
         }
@@ -42,15 +41,23 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    // This is recommended only for GBP payments where the majority of providers support App2App
     private fun loadCustomTab(url: String) {
-        println("Loading Custom Tab")
-        val builder = CustomTabsIntent.Builder()
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(this, Uri.parse(url))
+        try {
+            // Attempts to start custom tab with Google Chrome
+            val builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.intent.setPackage("com.android.chrome")
+            customTabsIntent.launchUrl(this, Uri.parse(url))
+        } catch (e: Exception) {
+            // If Google Chrome isn't available then uses the default browser
+            val builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(this, Uri.parse(url))
+        }
     }
 
-    private fun getLink() : String {
+    private fun getLink(): String {
         return textInput.text.toString()
     }
-
 }
